1b9bf1f2fe38dea21ad91525db4ca67ad93791e7
Artem Hresko <gresko.artem09@gmail.com>
2025-06-19 11:53:03 +0100

    /

---
+++ js/index.js
@@ -0,0 +1,78 @@
+// // // // 1
+
+let i = 0;
+
+while (i <= 10) {
+  console.log(i);
+  i = i + 1;
+}
+
+
+
+
+
+
+
+// // // 2
+
+for (let i = 2; i <= 20; i = i + 1) {
+  if (i % 2 != 0) continue;
+  else if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+
+
+
+
+
+
+// // 3
+
+let number = 7;
+
+for (let i = 0;
+    i <= 10;
+    i += 1) {
+    const table = number * i;
+    console.log(`${i} : ${table}`);
+}
+
+
+
+
+
+
+
+// 4
+
+let a = 0;
+let n = 15;
+
+
+
+
+
+while (a < n) {
+    a += 1;
+    if (a >= n)
+        break;
+    console.log(a);
+}
+
+
+
+
+
+// 5
+
+let number1 = 0;
+
+while (number1 <= 20) {
+
+    number1 += 1;
+  if (number1 % 3 === 0)
+        continue;
+  console.log(number1);
+}
